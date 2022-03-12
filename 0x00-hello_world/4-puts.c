2799e57da8c9fd7ddfdb4908ed609c278d715f93
Oreofe <oreofeolayemi@gmail.com>
2022-03-12 10:17:07 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return: 0 if success.
 
  */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+int main(void) 
+{  
+puts("\"Programming is like building a multilingual puzzle");  
 return (0);
 }
