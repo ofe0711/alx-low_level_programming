7580f8703e9f7b6aa98f73d35a194d4f5ddb7814
Oreofe <oreofeolayemi@gmail.com>
2022-03-12 10:26:05 -0800

    modified 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 * Description: main - Prints out a string to stdout.
 * Return: 0 if success.
 */
-int main(void) 
-{  
-puts("\"Programming is like building a multilingual puzzle");  
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
