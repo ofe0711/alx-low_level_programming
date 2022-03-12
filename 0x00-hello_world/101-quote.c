fbccd458003be3c1980f462179842d7ad3e8ba32
Oreofe <oreofeolayemi@gmail.com>
2022-03-12 10:39:19 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";  
+fprintf(stderr, "%s\n", n);  
+return (1);
+}
