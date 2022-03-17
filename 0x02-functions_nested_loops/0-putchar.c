1b0407bc546b82fc6ef4952b69c80e36c5dec57f
Oreofe <oreofeolayemi@gmail.com>
2022-03-17 14:02:17 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "putchar.c"
+/**
+* main - program that prints Holberton.
+*
+* Return: int
+*/
+int main(void)
+{
+int n = 0;
+char str_hbt[9] = "Putchar";
+while (n < 9)
+{
+_putchar(str_hbt[n]);
+n += 1;
+}
+_putchar('\n');
+return (0);
+}
