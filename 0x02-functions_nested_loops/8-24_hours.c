d16626a1fd51cc7e35aa3f4b06b8e8e88a3723e4
Oreofe <oreofeolayemi@gmail.com>
2022-03-18 03:59:39 -0700

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+void jack_bauer(void)
+{
+int x = 0;
+int j = 0;
+while (x <= 23)
+{
+j = 0;
+while (j <= 59)
+{
+_putchar((x / 10) + '0');	  
+_putchar((x % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');	  
+_putchar('\n');
+j += 1;
+}
+x += 1;
+}
+}
