03e1a550f57cd5ed6dd46175848abad103ba8726
Oreofe <oreofeolayemi@gmail.com>
2022-05-05 15:03:21 -0700

    modified

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
 *
 * Return: 1 if it worked, -1 if it didn't.
 */
-int clear_bit(unsigned long int *n, unsigned int index)
+xint clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned int m;
 if (index > 63)
